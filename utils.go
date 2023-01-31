08c13ada4e9873be374c24a24af98952128678e8
Jorrit Stutterheim <jorrit@Jorrits-MBP.home>
2023-01-31 20:34:32 +0100

    connectionstring

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package GoLib
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func ReadFile(fileName string) (string, error) {
@@ -17,11 +18,11 @@ func GetAMQConnectionString() (string, error) {
 	user := os.Getenv("AMQ_USER")
 	pwFile := os.Getenv("AMQ_PASSWORD_FILE")
 	pw, err := ReadFile(pwFile)
-
 	if err != nil {
 		return "", err
 	}
 
-	return fmt.Sprintf("amqp://%s:%s@rabbit:5672/", user, pw), nil
+	pw = strings.TrimSuffix(pw, "\n")
 
+	return fmt.Sprintf("amqp://%s:%s@rabbit:5672/", user, pw), nil
 }
