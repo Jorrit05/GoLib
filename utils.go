5d517c47fa9e4e993e5b941368a060eb1f1d3b48
Jorrit Stutterheim <jorrit@Jorrits-MBP.home>
2023-01-31 19:54:09 +0100

    filelog?

---
+++ utils.go
@@ -0,0 +1,27 @@
+package GoLib
+
+import (
+	"fmt"
+	"os"
+)
+
+func ReadFile(fileName string) (string, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		return string(""), err
+	}
+	return string(data), nil
+}
+
+func GetAMQConnectionString() (string, error) {
+	user := os.Getenv("AMQ_USER")
+	pwFile := os.Getenv("AMQ_PASSWORD_FILE")
+	pw, err := ReadFile(pwFile)
+
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("amqp://%s:%s@rabbit:5672/", user, pw), nil
+
+}
