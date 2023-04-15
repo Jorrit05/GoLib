b586544af21ac7802b577700e3426a26b008d804
Jorrit05 <jorstut@hotmail.com>
2023-04-15 15:43:19 +0200

    Also return JSON

--- types_reasoner.go
+++ types_reasoner.go
@@ -2,33 +2,33 @@ package GoLib
 
 var _ NameGetter = (*Requestor)(nil)
 
-type Iterable interface {
-	Len() int
-	Get(index int) interface{}
-}
+// type Iterable interface {
+// 	Len() int
+// 	Get(index int) interface{}
+// }
 
-type NameGetter interface {
-	GetName() string
-}
+// type NameGetter interface {
+// 	GetName() string
+// }
 
-type Requestor struct {
-	Name             string   `json:"name"`
-	CurrentArchetype string   `json:"current_archetype"`
-	AllowedPartners  []string `json:"allowed_partners"`
-}
+// type Requestor struct {
+// 	Name             string   `json:"name"`
+// 	CurrentArchetype string   `json:"current_archetype"`
+// 	AllowedPartners  []string `json:"allowed_partners"`
+// }
 
-type RequestorConfig struct {
-	Contents []Requestor `json:"requestor_config"`
-}
+// type RequestorConfig struct {
+// 	Contents []Requestor `json:"requestor_config"`
+// }
 
-func (c *RequestorConfig) Len() int {
-	return len(c.Contents)
-}
+// func (c *RequestorConfig) Len() int {
+// 	return len(c.Contents)
+// }
 
-func (c *RequestorConfig) Get(index int) interface{} {
-	return c.Contents[index]
-}
+// func (c *RequestorConfig) Get(index int) interface{} {
+// 	return &c.Contents[index]
+// }
 
-func (a *Requestor) GetName() string {
-	return a.Name
-}
+// func (a *Requestor) GetName() string {
+// 	return a.Name
+// }
